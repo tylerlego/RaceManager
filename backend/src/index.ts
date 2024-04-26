64e277739909cd073a4df913a30896612b195dcb
Tyler Lego <tylerlego15@gmail.com>
2024-04-26 15:47:13 -0400

    set cors to allow all origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,16 +21,15 @@ const app: Express = express();
 const port = process.env.EXPRESS_PORT || 4000;
 
 const corsOrigins = [
-  process.env.LOCAL_CLIENT_BASE_URL + '*' || '',
-  process.env.CLIENT_BASE_URL + '*' || '',
-  process.env.LOCAL_SERVER_BASE_URL + '*' || '',
-  process.env.SERVER_BASE_URL + '*' || '',
-  '*'
+  process.env.LOCAL_CLIENT_BASE_URL || '',
+  process.env.CLIENT_BASE_URL || '',
+  process.env.LOCAL_SERVER_BASE_URL || '',
+  process.env.SERVER_BASE_URL || '',
 ]
 console.log("cors origins", corsOrigins);
 
 app.use(cors({
-  origin: corsOrigins,
+  origin: true,
   credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
